f4095a221a96c0a7bb1f7dce4556622c44502a8b
seffeng <seffeng@sina.cn>
2026-03-24 10:19:34 +0800

    fix

--- config/database.php
+++ config/database.php
@@ -65,7 +65,7 @@ return [
             'strict' => env('DB_STRICT_MODE', true),
             'engine' => env('DB_ENGINE', null),
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA')
             ]) : []
         ],
 
